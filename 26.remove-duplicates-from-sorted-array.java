8e2bc5d5051237fdf055434917cccb4cb3640f5b
Chinmayi <cujoshi919@gmail.com>
2024-06-05 12:58:17 -0400

    sol1

--- 26.remove-duplicates-from-sorted-array.java
+++ 26.remove-duplicates-from-sorted-array.java
@@ -7,7 +7,18 @@
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
+        int l=0,r=1,n;
+        n=nums.length;
+        while(r<n){
+            if(nums[l]==nums[r])
+                r++;
+            else{
+                l++;
+                nums[l]=nums[r];
+                r++;
+            }
+        }
+    return l+1;
     }
 }
 // @lc code=end
