9be32616a1aec84ee17a3f119b6f6248735a2554
Chinmayi <cujoshi919@gmail.com>
2024-06-05 15:00:23 -0400

    sol11

--- 977.squares-of-a-sorted-array.java
+++ 977.squares-of-a-sorted-array.java
@@ -7,7 +7,30 @@
 // @lc code=start
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        
+        int n=nums.length;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+
+        int l=0,r=n-1;
+        for(int j=n-1;j>=0;j--){
+            if(nums[l]<nums[r]){
+                arr[j]=nums[r];
+                r--;
+            }
+            else{
+                arr[j]=nums[l];
+                l++;                
+            }
+            // else{
+            //     arr[j]=nums[l];
+            //     //arr[j-1]=nums[r];
+            //     l++;
+            //     r--;
+            // }
+        }
+        return arr;
     }
 }
 // @lc code=end
